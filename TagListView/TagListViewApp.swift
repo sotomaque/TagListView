502059c72ec54c94154b4448151b8902f92bf402
Enrique <sotomaque@gmail.com>
2021-10-09 18:33:24 -0700

    Initial Commit

---
+++ TagListView/TagListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagListViewApp.swift
+//  TagListView
+//
+//  Created by Enrique Sotomayor on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TagListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
